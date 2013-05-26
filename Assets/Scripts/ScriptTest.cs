e0368f979cc570b02db223f92614e94e4ec0316c
Tim Chang <kw0006667@hotmail.com>
2013-05-26 20:05:47 +0800

    test
    test

---
+++ Assets/Scripts/ScriptTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScriptTest : MonoBehaviour {
+
+#pragma 
+    public GameObject Ojjj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
